5a3518b497d643398bfbaaca082c92a4eb7545b9
tg-bots-ish <mr.shket228@yandex.ru>
2019-09-11 21:11:49 +0300

    debug

--- example.py
+++ example.py
@@ -5,6 +5,8 @@ import telebot
 TOKEN = "734820541:AAEkVd8f-haWGhYPmiB3AHXbJtAqMOcNWqY"
 bot = telebot.TeleBot(TOKEN)
 
+white_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] + ['*', '/', '+', '-', '%', ' ']
+
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
@@ -15,13 +17,20 @@ def start_handler(message):
 def text_handler(message):
     text = message.text.lower()
     chat_id = message.chat.id
-    try:
-        print("I want to do it")
-        result = literal_eval(text)
-        bot.send_message(chat_id, text + '=' + result)
-    except:
-        print("I cant")
+    good = all(map(lambda x: x in white_list, text))
+    print(good, text, list(map(lambda x: x in white_list, text)))
+
+    if not good:
+        print("It was not good")
         bot.send_message(chat_id, "I can't understand")
+    else:
+        try:
+            print("I want to do it")
+            result = literal_eval(text)
+            bot.send_message(chat_id, text + '=' + result)
+        except:
+            print("I cant")
+            bot.send_message(chat_id, "I can't understand")
 
 
 bot.polling(none_stop=True)
